a28ecd0384779aa9f26fe0dd3bd660c15d600b40
Rosen Dimitrov <rossen.dimitrov@fastcomet.com>
2021-01-15 14:19:41 +0200

    1.0.2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mix = require('laravel-mix');
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
-const ignoredFiles = ['resources/js/client/app.js', 'resources/js/admin/app.js'];
 
 mix.vue().webpackConfig({'plugins': [
   new webpack.IgnorePlugin({
@@ -11,7 +10,7 @@ mix.vue().webpackConfig({'plugins': [
   })
 ]});
 
-const mixDir = (method, source, output) => {
+const mixDir = (method, source, output, ignoredFiles = []) => {
   source += (source.slice(-1) === '/' ? '' : '/');
   output += (output.slice(-1) === '/' ? '' : '/');
   fs.readdirSync(source).forEach(file => {
